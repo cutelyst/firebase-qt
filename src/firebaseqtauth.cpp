4687b7f6e0f096370b877a3b26fd2acacbab42a7
Daniel Nicoletti <dantti12@gmail.com>
2024-10-03 15:30:18 -0300

    Fix build

--- src/firebaseqtauth.cpp
+++ src/firebaseqtauth.cpp
@@ -10,19 +10,21 @@
 #include <QThread>
 #include <QLoggingCategory>
 
-class FirebaseQtAuthStateListener : public firebase::auth::AuthStateListener {
+using namespace firebase;
+
+class FirebaseQtAuthStateListener : public auth::AuthStateListener {
  public:
-  void OnAuthStateChanged(firebase::auth::Auth* auth) override {
-    firebase::auth::User* user = auth->current_user();
-    if (user != nullptr) {
+  void OnAuthStateChanged(auth::Auth* auth) override {
+    auth::User user = auth->current_user();
+    if (user.is_valid()) {
       // User is signed in
-      printf("OnAuthStateChanged: signed_in %s\n", user->uid().c_str());
-      const std::string displayName = user->display_name();
-      const std::string emailAddress = user->email();
-      const std::string photoUrl = user->photo_url();
+      qDebug("OnAuthStateChanged: signed_in %s\n", user.uid().c_str());
+      const std::string displayName = user.display_name();
+      const std::string emailAddress = user.email();
+      const std::string photoUrl = user.photo_url();
     } else {
       // User is signed out
-      printf("OnAuthStateChanged: signed_out\n");
+      qDebug("OnAuthStateChanged: signed_out\n");
     }
     // ...
   }
@@ -43,9 +45,9 @@ FirebaseQtAuth::~FirebaseQtAuth()
 void FirebaseQtAuth::initialize(FirebaseQtApp *app)
 {
     Q_D(FirebaseQtAuth);
-    firebase::InitResult result;
-    d->auth = firebase::auth::Auth::GetAuth(app->d_ptr->app, &result);
-    Q_ASSERT_X(result == firebase::InitResult::kInitResultSuccess, "FirebaseQtAuth::initialize()", "result");
+    InitResult result;
+    d->auth = auth::Auth::GetAuth(app->d_ptr->app, &result);
+    Q_ASSERT_X(result == InitResult::kInitResultSuccess, "FirebaseQtAuth::initialize()", "result");
     d->stateListener = new FirebaseQtAuthStateListener;
     d->auth->AddAuthStateListener(d->stateListener);
 }
@@ -54,21 +56,21 @@ void FirebaseQtAuth::signIn(const FirebaseQtAuthCredential &credential)
 {
     Q_D(FirebaseQtAuth);
     qDebug() << "signIn" << QThread::currentThreadId();
-    firebase::Future<firebase::auth::User*> future = d->auth->SignInWithCredential(credential.d->credential);
-    future.OnCompletion([=] (const firebase::Future<firebase::auth::User*> &result) {
+    Future<auth::User> future = d->auth->SignInWithCredential(credential.d->credential);
+    future.OnCompletion([=] (const Future<auth::User>& result) {
         if (result.error()) {
             qWarning() << "signIn future error" << result.error() << result.error_message() << result.status() << result.result();
             Q_EMIT signInError(result.error(), QString::fromStdString(result.error_message()));
         } else {
-            firebase::auth::User *user = *result.result();
-            firebase::Future<std::string> f2 = user->GetToken(false);
-            f2.OnCompletion([=] (const firebase::Future<std::string> &r2) {
+            auth::User user = *result.result();
+            Future<std::string> f2 = user.GetToken(false);
+            f2.OnCompletion([=] (const Future<std::string>& r2) {
                 if (result.error()) {
                     qWarning() << "get token future error" << result.error() << r2.error_message() << r2.status() << r2.result();
                     Q_EMIT signInError(result.error(), QString::fromStdString(r2.error_message()));
                 } else {
                     std::string token = *r2.result();
-                    qDebug() << "get token future" << user->uid().c_str() << token.c_str();
+                    qDebug() << "get token future" << user.uid().c_str() << token.c_str();
                     Q_EMIT signInToken(QString::fromStdString(token));
                 }
             });
